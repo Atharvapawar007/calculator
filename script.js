3b312509694d39150436ca142df572b0c8771f7e
Atharva Pawar <164200355+Atharvapawar007@users.noreply.github.com>
2026-07-12 05:37:29 +0000

    created the skeleton of js file

--- script.js
+++ script.js
@@ -1,74 +1,47 @@
+//input
 const input = document.querySelector("input");
-input.value = 0;
+input.value = "0";
 
-let num1 = 0;
-let num2 = undefined;
-let opt = undefined;
-
+//buttons
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
-const clearButton = document.querySelector(".reset");
-const answerButton = document.querySelector(".answer");
+const equalToButton = document.querySelector("#answer");
+const backspaceButton = document.querySelector("#backspace");
+const decimalButton = document.querySelector("#decimal");
+const clearButton = document.querySelector("#reset");
 
+//operations
+
+//1. number buttons
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener("click", () => {
-        const number = numberButton.innerText;
 
-        if(input.value === "0" || input.value === "+" || input.value === "-" || input.value === "×" || input.value === "÷"){
-            input.value = "";
-        }
-        input.value += number;
-
-        if(opt === undefined){
-            num1 = input.value;
-        }else{
-            num2 = input.value;
-        }
     })
 })
 
+//2. operator buttons
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener("click", () => {
-        const operator = operatorButton.innerText;
-        opt = operator;
-        input.value = opt;
+
     })
 })
 
-clearButton.addEventListener("click", () => {
-    input.value = "0";
-    num1 = 0;
-    num2 = undefined;
-    opt = undefined;
+//3. equalTo button
+equalToButton.addEventListener("click", () => {
+
 })
 
-answerButton.addEventListener("click", () => {
-    if(opt === undefined && num2 === undefined){
-        input.value = num1;
-    }else if(num2 === undefined){
-        input.value = "ERROR!";
-    }else{
-        let val1 = Number(num1);
-        let val2 = Number(num2);
+//4. backspace button
+backspaceButton.addEventListener("click", () => {
 
-        let answer = 0;
-        if(opt === "+"){
-            answer = val1 + val2;
-        }else if(opt === "-"){
-            answer = val1 - val2;
-        }else if(opt === "×"){
-            answer = val1 * val2;
-        }else{
-            if(val2 === 0){
-                answer = "ERROR!"
-            }else{
-                answer = val1 / val2;
-            }
-        }
-        input.value = answer;
-    }
-    hasAnswered = true;
-    num1 = 0;
-    num2 = undefined;
-    opt = undefined;
+})
+
+//5. dedcimal button
+decimalButton.addEventListener("click", () => {
+
+})
+
+//6. clear button
+clearButton.addEventListener("click", () => {
+
 })
