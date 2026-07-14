c90e8045df74d76241296d3485cfa9f63748825d
Atharva Pawar <164200355+Atharvapawar007@users.noreply.github.com>
2026-07-14 03:57:00 +0000

    created the basic functioning, now only variable allotment is left

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ const backspaceButton = document.querySelector("#backspace");
 const decimalButton = document.querySelector("#decimal");
 const clearButton = document.querySelector("#reset");
 
-//operations
+//variable
+let num1 = 0;
+let num2 = undefined;
+let operator = undefined;
 
+//operations
 //1. number buttons
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener("click", () => {
+        //if there's a 0 on the input screen
+        if(input.value === "0"){
+            input.value = "";
+        }
 
+        //if an operator is already selected
+        const opt = document.querySelector(".selected");
+        if(opt !== null){
+            opt.classList.remove("selected");
+            input.value = "";
+        }
+
+        const number = numberButton.innerText;
+        input.value += number;
     })
 })
 
@@ -23,25 +40,79 @@ numberButtons.forEach(numberButton => {
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener("click", () => {
 
+        //If the same operator is already selected
+        if(operatorButton.classList.contains("selected")){
+            operatorButton.classList.remove("selected");
+            operator = undefined;
+        }else{
+            //if an operator is already selected
+            const opt = document.querySelector(".selected");
+            if(opt !== null){
+                opt.classList.remove("selected");
+            }
+            
+            //select the operator
+            operatorButton.classList.add("selected");
+            operator = operatorButton.innerText;
+        }
     })
 })
 
 //3. equalTo button
 equalToButton.addEventListener("click", () => {
-
+    input.value = calculateAnswer(num1, num2, operator);
 })
 
 //4. backspace button
 backspaceButton.addEventListener("click", () => {
-
+    input.value = input.value.slice(0, -1);
 })
 
 //5. dedcimal button
 decimalButton.addEventListener("click", () => {
-
+    let number = input.value;
+    if(number !== "" && !number.includes(".")){
+        number += ".";
+        input.value = number;
+    }
 })
 
 //6. clear button
 clearButton.addEventListener("click", () => {
-
+    //default state
+    input.value = "0";
+    num1 = 0;
+    num2 = undefined;
+    operator = undefined;
+    
+    //if an operator is already selected
+    const opt = document.querySelector(".selected");
+    if(opt !== null){
+        opt.classList.remove("selected")
+    }
 })
+
+//operational logic
+function calculateAnswer(number1, number2, operator){
+    if(number2 === undefined || operator === undefined){
+        input.value = "ERROR!";
+    }else{
+        const val1 = Number(number1);
+        const val2 = Number(number2);
+
+        if(operator === "+"){
+            return val1 + val2;
+        }else if(operator === "-"){
+            return val1 - val2;
+        }else if(operator === "×"){
+            return val1 * val2;
+        }else{
+            if(num2 == 0){
+                return "ERROR!"
+            }else{
+                const result = num1 / num2;
+                return result.toFixed(4);
+            }
+        }
+    }
+}
